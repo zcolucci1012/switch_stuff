dcb7c2c07907cd8ae87a4e42ba0d40ac0b483d2b
zcolucci1012 <zacharycolucci@gmail.com>
2019-07-13 23:45:09 -0400

    Update random_char.js

--- random_char.js
+++ random_char.js
@@ -2,7 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var characters = JSON.parse(this.responseText);
-    alert(characters[0]);
+    alert(characters[0].name);
     //document.getElementById("demo").innerHTML = myObj.name;
   }
 };
