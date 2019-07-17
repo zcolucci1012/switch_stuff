f7d176c3a17e5de339e5eb0952425692bb566dcc
zcolucci1012 <zacharycolucci@gmail.com>
2019-07-17 15:25:18 -0400

    Testing

--- random_char.js
+++ random_char.js
@@ -1,9 +1,22 @@
+var invalid_id = ["74", "75"];
+var continue = true;
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var characters = JSON.parse(this.responseText);
-    alert(characters[0].id);
-    alert(characters.length)
+    var character;
+    if (continue) {
+      character = characters[Math.floor(Math.random() * characters.length)];
+      continue = false;
+      for (var i=0; i<invalid.length; i++){
+        if (invalid[i] == character.id){
+          continue = true;
+          break;
+        }
+      }
+    }
+    alert(character.id + character.name);
   }
 };
 xmlhttp.open("GET", "characters.json", true);
