4b32f89969ebd333f4af99d47a0dd0097b82cca1
zcolucci1012 <zacharycolucci@gmail.com>
2019-07-17 15:28:56 -0400

    Update random_char.js

--- random_char.js
+++ random_char.js
@@ -1,17 +1,17 @@
 var invalid_id = ["74", "75"];
-var continue = true;
+var proceed = true;
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var characters = JSON.parse(this.responseText);
     var character;
-    if (continue) {
+    if (proceed) {
       character = characters[Math.floor(Math.random() * characters.length)];
-      continue = false;
+      proceed = false;
       for (var i=0; i<invalid.length; i++){
         if (invalid[i] == character.id){
-          continue = true;
+          proceed = true;
           break;
         }
       }
