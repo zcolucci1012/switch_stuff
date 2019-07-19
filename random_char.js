7a3a02db6cdf2fab5e62dfe4dc759c999a7591c8
zcolucci1012 <zacharycolucci@gmail.com>
2019-07-18 21:23:32 -0400

    Update random_char.js

--- random_char.js
+++ random_char.js
@@ -1,29 +1,31 @@
 var invalid_id = ["72", "73", "74", "75", "33", "34", "35"];
 var proceed = true;
-function generate(){
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var characters = JSON.parse(this.responseText);
-      var character;
-      while (proceed) {
-        character = characters[Math.floor(Math.random() * characters.length)];
-        proceed = false;
-        for (var i=0; i<invalid_id.length; i++){
-          if (invalid_id[i] == character.id){
-            proceed = true;
-            break;
-          }
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var characters = JSON.parse(this.responseText);
+    var character;
+    while (proceed) {
+      character = characters[Math.floor(Math.random() * characters.length)];
+      proceed = false;
+      for (var i=0; i<invalid_id.length; i++){
+        if (invalid_id[i] == character.id){
+          proceed = true;
+          break;
         }
       }
-      var id = document.getElementById("id");
-      id.innerHTML = character.id;
-      var name = document.getElementById("name");
-      name.innerHTML = character.name;
-      var picture = document.getElementById("picture")
-      picture.setAttribute("src", character.img)
     }
-  };
+    var id = document.getElementById("id");
+    id.innerHTML = character.id;
+    var name = document.getElementById("name");
+    name.innerHTML = character.name;
+    var picture = document.getElementById("picture")
+    picture.setAttribute("src", character.img)
+  }
+};
+
+function generate(){
   xmlhttp.open("GET", "characters.json", true);
   xmlhttp.send();
 }
