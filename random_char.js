0dac6f4e3a276da08f0b42bdf95d5fc4f57139f0
zcolucci1012 <zacharycolucci@gmail.com>
2019-07-18 21:16:32 -0400

    Added Generate button and Settings

--- random_char.js
+++ random_char.js
@@ -1,28 +1,57 @@
-var invalid_id = ["72", "73", "74", "75"];
+var invalid_id = ["72", "73", "74", "75", "33", "34", "35"];
 var proceed = true;
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var characters = JSON.parse(this.responseText);
-    var character;
-    while (proceed) {
-      character = characters[Math.floor(Math.random() * characters.length)];
-      proceed = false;
-      for (var i=0; i<invalid_id.length; i++){
-        if (invalid_id[i] == character.id){
-          proceed = true;
-          break;
+function generate(){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var characters = JSON.parse(this.responseText);
+      var character;
+      while (proceed) {
+        character = characters[Math.floor(Math.random() * characters.length)];
+        proceed = false;
+        for (var i=0; i<invalid_id.length; i++){
+          if (invalid_id[i] == character.id){
+            proceed = true;
+            break;
+          }
         }
       }
+      var id = document.getElementById("id");
+      id.innerHTML = character.id;
+      var name = document.getElementById("name");
+      name.innerHTML = character.name;
+      var picture = document.getElementById("picture")
+      picture.setAttribute("src", character.img)
     }
-    var id = document.getElementById("id");
-    id.innerHTML = character.id;
-    var name = document.getElementById("name");
-    name.innerHTML = character.name;
-    var picture = document.getElementById("picture")
-    picture.setAttribute("src", character.img)
+  };
+  xmlhttp.open("GET", "characters.json", true);
+  xmlhttp.send();
+}
+
+var timer = window.setInterval(function(){
+  checkInputs()
+}, 1);
+
+
+function checkInputs(){
+  var echo_fighters = document.getElementById("echo_fighters")
+  var pokemon_trainer = document.getElementById("pokemon_trainer")
+
+  invalid_id = ["72", "73", "74", "75"]
+
+  if (echo_fighters.checked){
+    var echoes = ["4e", "13e", "21e", "25e", "28e", "60e", "66e"]
+    for (var i=0; i<echoes.length; i++){
+      invalid_id.push(echoes[i]);
+    }
   }
-};
-xmlhttp.open("GET", "characters.json", true);
-xmlhttp.send();
+  if (pokemon_trainer.checked){
+    invalid_id.push("33-35")
+  }
+  else {
+    var pokemon = ["33", "34", "35"]
+    for (var i=0; i<pokemon_trainer.length; i++){
+      invalid_id.push(pokemon_trainer[i]);
+    }
+  }
+}
